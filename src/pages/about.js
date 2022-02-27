dfe20146d61793d47cb94926521e551d5d4d0834
Renju Jose <renjujv@gmail.com>
2022-02-27 17:48:03 +0530

    Update layout
    - split bio and social icons
    - about page layout
    - stick rss link to right
    - Remove Bio from landing page header

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import Bio from '../components/bio';
 
 const aboutPage = () => {
-    return <main>
+    return <main className='global-wrapper'>
         <title>About Me</title>
         <h1>About Me</h1>
         <p>I am a full stack Web Developer by profession and a tech enthusiast by nature.</p>
+        <Bio />
     </main>
 }
 
