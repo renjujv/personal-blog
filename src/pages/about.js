7b0836f37598028b01e8b69bf90471b734eacaa7
Renju Jose <renjujv@gmail.com>
2022-02-27 19:34:56 +0530

    Update title and about

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,12 @@ const aboutPage = () => {
     return <main className='global-wrapper'>
         <title>About Me</title>
         <h1>About Me</h1>
-        <p>I am a full stack Web Developer by profession and a tech enthusiast by nature.</p>
+        <p>I am a full stack Web Developer by profession and a tech enthusiast by nature.
+            I am what they call 'Jack of all trades'.
+        </p>
+        <p>Before you get critical, let me help you with the full form:
+            <pre>A jack of all trades is a master of none, but oftentimes better than a master of one.</pre>
+        </p>
         <Bio />
     </main>
 }
