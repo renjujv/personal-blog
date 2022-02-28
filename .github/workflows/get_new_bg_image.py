b679d4dadc00532a549209b1eb543863bdeef12b
Renju Jose <renjujv@gmail.com>
2022-02-28 19:59:03 +0530

    Add python script

--- .github/workflows/get_new_bg_image.py
+++ .github/workflows/get_new_bg_image.py
@@ -2,8 +2,8 @@ import json,sys,urllib3;
 
 http = urllib3.PoolManager()
 response = http.request('GET', 'https://api.unsplash.com/photos/random?client_id=DBmWBkEw9XM-RiyKjTitrQuPvzZtbd2FtClQX1a1an0')
-print response.data
+print(response.data)
 obj=json.load(response.data)
 image_url = obj['urls.regular'])
-print image_url
+print(image_url)
 Img = http.request('GET',image_url)
